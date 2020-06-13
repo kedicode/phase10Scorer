4c8e167ae722f6189c1a52ae6b0b75cbbf2db26c
kedicode <kedicode@gmail.com>
2020-06-12 23:34:41 -0400

    add game and player class

---
+++ Pages/Models/Game.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace phase10Scorer.Models
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; }
+    }
+}
---
+++ Pages/Models/Player.cs
@@ -0,0 +1,7 @@
+namespace phase10Scorer.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+    }
+}
